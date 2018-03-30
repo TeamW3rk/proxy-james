67594e1aabdda180f3ea6fbde2294e3d75546651
James Powers <powersajames@gmail.com>
2018-03-30 13:17:32 -0700

    rework for SSR

---
+++ helpers/bundles.js
@@ -0,0 +1,16 @@
+module.exports =
+[
+  {
+    service: 'reviews',
+    url: `http://localhost:4004/`,
+    endpoint: 'reviews',
+    fileName: {
+      server: 'bundle-server.js',
+      client: 'bundle.js',
+    },
+    props: {
+      id: 'restId',
+      data: 'restaurant',
+    }
+  }
+];
